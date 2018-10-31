c9485aebcd14db17d03a154c1ac8c6f827418dd9
I Putu Iduar Perdana <37173696+iduarperdana@users.noreply.github.com>
2018-10-31 10:39:14 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+</head>
+<body>
+
+	
+<!-- Navbar goes here -->
+ <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center">Logo</a>
+    </div>
+  </nav>
+        
+  
+  <nav>
+    <div class="nav-wrapper  pink darken-1">
+      <form action="index.php" method="POST">
+        <div class="input-field">
+          <input id="search" type="search" name="search" required>
+          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
+    </div>
+  </nav>
+
+  <table>
+        <thead>
+          <tr>
+              <th>NO</th>
+              <th>Nama Buku</th>
+              <th>Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <?php
+							// Dipanggil fungsi tokenisasi
+							// $stats = tokenisasi($content);
+
+							$i=1;
+							$kalimat = $_POST['search'] ;
+$kalimat_array = explode(" ",$kalimat);
+foreach($kalimat_array as $value)
+{
+    if(isset($str_count[$value]))
+        $str_count[$value]++;
+    else
+        $str_count[$value]=1;
+}
+							foreach($str_count as $key => $value) {
+
+								// if term != "", supaya gak diindex term yg kosong
+								if($key != ""){
+						?>
+						<tr>
+							<td><?php echo $i ?></td>
+							<td><?php echo $key ?></td>
+							<td><?php echo $value ?></td>
+							
+						</tr>
+						<?php
+									$i++;
+								}
+							}
+						?>
+          </tr>
+        
+        </tbody>
+      </table>
+
+
+</body>
+</html>
+
+<?php
+
+
+
+   
+
+
+//echo "My first PHP script!";
+?>
