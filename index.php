65eaab82c60bbc11338983d24936223dc096afa2
I Putu Iduar Perdana <37173696+iduarperdana@users.noreply.github.com>
2018-11-01 09:23:00 +0800

    Paperpus
    token

--- index.php
+++ index.php
@@ -10,14 +10,14 @@
 <!-- Navbar goes here -->
  <nav>
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo center">Logo</a>
+      <a href="#" class="brand-logo center"><img src="img/logo.png" style="width:45px;height:45px; margin-top: 10px;"> Paperpus</a>
     </div>
   </nav>
         
   
   <nav>
     <div class="nav-wrapper  pink darken-1">
-      <form action="index.php" method="POST">
+      <form action="index.php" method="GET">
         <div class="input-field">
           <input id="search" type="search" name="search" required>
           <label class="label-icon" for="search"><i class="material-icons">search</i></label>
@@ -40,16 +40,14 @@
             <?php
 							// Dipanggil fungsi tokenisasi
 							// $stats = tokenisasi($content);
-
-							$i=1;
-							$kalimat = $_POST['search'] ;
-$kalimat_array = explode(" ",$kalimat);
-foreach($kalimat_array as $value)
-{
-    if(isset($str_count[$value]))
-        $str_count[$value]++;
-    else
-        $str_count[$value]=1;
+            $i=1;
+            $kalimat = isset($_GET['search']) ? $_GET['search']: '';
+            $kalimat_array = explode(" ",$kalimat);
+				foreach($kalimat_array as $value){
+    				if(isset($str_count[$value]))
+    					$str_count[$value]++;
+    				else
+    					$str_count[$value]=1;
 }
 							foreach($str_count as $key => $value) {
 
